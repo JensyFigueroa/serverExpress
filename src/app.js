a089910b3b232f6a3ea9976128c5f75f12887b00
Jensy Figueroa <jensy1418@gmail.com>
2023-09-17 19:01:30 -0400

    First commit

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
-const express = require('express')
+const express = require("express");
 
-const server = express()
+const server = express();
 
-server.get('/',(req, res) => {
-  res.send('server started')
-}
-)
+//Para web static
+server.use(express.static(__dirname + "/public"));
 
-module.exports = server
+server.get("/", (req, res) => {
+  res.send("server started");
+});
+
+server.get("/service", (req, res) => {
+  res.send("Page service");
+});
+
+module.exports = server;
