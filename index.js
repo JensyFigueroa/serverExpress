0d164e624ee6f5ae2b104b02ebea3814ebc8f2f5
Jensy Figueroa <jensy1418@gmail.com>
2023-09-21 19:50:01 -0400

    the .env configuration was made

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const server = require('./src/app')
+require('dotenv').config();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log('listening on port ' + PORT)
 )
--- src/db.js
+++ src/db.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
 
-const user = "jensy1418";
-const password = "pfFwyAPDw19aT5ND";
-const dbname = 'vet'
-const uri = `mongodb+srv://${user}:${password}@cluster0.4rgxytc.mongodb.net/${dbname}?retryWrites=true&w=majority";`;
+require('dotenv').config();
+const {USER, PASSWORD, DBNAME} = process.env
+
+
+// const user = "jensy1418";
+// const password = "pfFwyAPDw19aT5ND";
+// const dbname = 'vet'
+const uri = `mongodb+srv://${USER}:${PASSWORD}@cluster0.4rgxytc.mongodb.net/${DBNAME}?retryWrites=true&w=majority";`;
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log('connected database...')
